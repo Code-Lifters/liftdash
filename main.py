d4c11b1bff2806013c972b1bdbfb41cb30b4e9a9
Toby Manders <toby.manders@MacBook-Pro.local>
2023-11-28 18:38:59 -0800

    gitignore, input sample and main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def create_plot():
+    # Read data from CSV
+    df = pd.read_csv('input/strong.csv')
+
+    # Example plot (modify according to your data)
+    plt.figure()
+    df.plot()  # Replace with your own plotting logic
+    plt.title('Sample Plot')
+
+    # Save plot to the images folder
+    plt.savefig('output/images/sample_plot.jpg', format='jpg')
+
+if __name__ == "__main__":
+    create_plot()
+
